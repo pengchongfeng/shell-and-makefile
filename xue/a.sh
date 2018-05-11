d8515e660b8a002a16447a41be991f8be77373d4
chongfeng.peng <chongfeng.peng@inin88.com>
2018-05-11 13:07:23 +0800

    modify a.sh

--- xue/a.sh
+++ xue/a.sh
@@ -26,6 +26,54 @@ readonly color
 #PATH="dd" #直接修改env中的PATH
 #unset a #从env删除了a
 
+echo "-----------------------------------"
+echo "我的pid:"
+echo $$ #shell进程自己的pid
 
+echo "我的名称:"
+echo $0 #脚本名称. $1第一个参数 $2第二个参数......
+echo "我的命令行参数:"
+echo $* #命令行参数 $1~$n  
+
+echo "命令行参数的个数（不包括自己）:"
+echo $# #命令行参数的个数（不包括自己）
+echo $@ #同$* 区别：$@每个参数是一个单词，循环执行语句。$*整体参数作为一个单词，一次执行语句。
+
+#运算expr 直接echo出来了
+expr 661 + 5
+
+test "peng" = "string"
+
+#运算符：= == != ! -o -a 其中-o对应or -a对应and
+#关系运算符:
+#-eq    equal                true
+#-ne    not equal            true
+#-gt    greater than         true
+#-lt    litter than          true
+#-ge    greater equal than   true
+#-le    litter equal than    true
+
+#字符串运算符：
+# =   字符串相等 true
+#!=   字符串不等 true
+#-z   空字符串   true
+#-n   非空字符串 true
+
+#检测文件特征：
+#-d file  目录？        true
+#-r file  可读？        true
+#-w file  可写？        true
+#-x file  可执行？      true
+#-s file  空文件？      true
+#-e file  文件/目录存在?true
+
+
+str1="deoxyribonucleic"
+str2="acid"
+#拼接字符串
+str=${str1}"-"${str2} #"deoxyribonucleic-acid"
+echo ${str}
+echo ${#str2} #str2长度
+echo ${str:2:4} #"oxyr" 即从0开始，和python切片一样
 
 
